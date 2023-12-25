45278673eaba0fa48edc5c7931fff5aa5840d869
sabari <balasabari27@gmail.com>
2023-12-25 12:56:32 +0530

    update code  12-56

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+  <Router>
+  <App />
+</Router>
   // {/* </React.StrictMode> */}
 );
 
