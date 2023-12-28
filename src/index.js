379fd0de6047f14118efef3333e15e90f28a62be
Sabari711 <balasabari27@gmail.com>
2023-12-28 18:15:23 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -25,9 +25,7 @@ import { BrowserRouter, HashRouter, Route,  Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <HashRouter> */}
       <App />
-    {/* </HashRouter> */}
   </React.StrictMode>
 );
 
